06cffd59039bbc1b38180caacc006794c69f375e
Cassitea <cassidynguyen3@gmail.com>
2022-04-29 19:24:42 -0400

    man idek

--- database.js
+++ database.js
@@ -8,25 +8,24 @@ if (row === undefined) {
     console.log('Log database appears to be empty. Creating log database...')
 
     const sqlInit = `
-        CREATE TABLE accesslog ( 
-            id INTEGER PRIMARY KEY, 
-            remoteaddr TEXT, 
-            remoteuser TEXT, 
-            date TEXT, 
-            method TEXT, 
-            url TEXT, 
-            protocol TEXT,
-            httpversion NUMERIC, 
-            status INTEGER, 
-            content_length NUMERIC,
-            referrer_url TEXT,
-            user_agent TEXT
-        );
-    `
+    CREATE TABLE accesslog ( 
+        id INTEGER PRIMARY KEY, 
+        remoteaddr TEXT,
+        remoteuser TEXT,
+        time TEXT,
+        method TEXT,
+        url TEXT,
+        protocol TEXT,
+        httpversion TEXT,
+        status TEXT, 
+        referrer TEXT,
+        useragent TEXT
+    );
+`
 
-    db.exec(sqlInit)
+db.exec(sqlInit)
 } else {
-    console.log('Log database exists.')
+console.log('Log database exists.')
 }
 
 module.exports = db
